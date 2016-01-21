0a56cc977246dbb0fb20f1c4b84444b06f9a7811
kallstadt <kallstadt@usgs.gov>
2016-01-21 16:54:09 -0700

    started to make some changes

--- runmodels.py
+++ runmodels.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
+#stdlib imports
+from configobj import ConfigObj
+
 #local imports
-from groundfailure.godt2008 import runmodel
+from groundfailure.newmark import godt2008
+from mapio.shake import ShakeGrid
 
 configfile = '/Users/kallstadt/SecondaryHazards/Codes/inputs/confignew.ini'
 shakefile = '/Users/kallstadt/SecondaryHazards/Northridge.xml'  # URL or filename
 
-maplayers, edict = runmodel(shakefile, configfile, mapshaking=True, mapmaxslope=True, mapcohesion=True, mapfriction=True, bounds='shakemap')  # Note that there are many options for mapping and saving outputs, being run as defaults here by giving no inputs
+edict = ShakeGrid.getEventDict(shakefile)
+config = ConfigObj(configfile)
+
+maplayers = godt2008(shakefile, config, saveinputs=True, regressionmodel='J_PGA')  # Note that there are many options for mapping and saving outputs, being run as defaults here by giving no inputs
+
+makeMap(maplayers, edict, configfile, modelname='Godt et al. 2008', maproads=maproads, mapcities=mapcities, colormaps=colormaps, plotorder=plotorder, boundaries=boundaries1)
+        #saveMap(maplayers, edict, modelname)
+
+# Need to add these to config file
+#plotorder = []
+#colormaps = []
