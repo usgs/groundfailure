27c2769282471fc62cfdea510f42550c5e034d51
kallstadt <kallstadt@usgs.gov>
2016-10-18 12:17:14 -0600

    tweaked setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+import os
 
 setup(name='groundfailure',
       version='0.1dev',
@@ -6,7 +7,10 @@ setup(name='groundfailure',
       author='Kate Allstadt, Mike Hearne, Eric Thompson, Katie Biegel',
       author_email='kallstadt@usgs.gov,mhearne@usgs.gov,emthompson@usgs.gov,kbiegel@usgs.gov',
       url='http://github.com/usgs/groundfailure',
-      packages=['groundfailure'],
-      package_data={'groundfailure': ['configspec.ini']},
+      packages=['groundfailure',
+                'tests'],
+      package_data={'groundfailure': ['configspec.ini',
+                                      os.path.join('tests', 'data', '*'),
+                                      ]},
       scripts=['gfail'],
       )
