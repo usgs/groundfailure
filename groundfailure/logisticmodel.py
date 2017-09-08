fixed bug in colorbar scaling for single key

groundfailure/logisticmodel.py
@@ -324,7 +324,7 @@ def quickcut(filename, tempname, gdict, extrasamp=5, method='nearest'):
     # Using subprocess approach because gdal.Translate doesn't hang on the command until the file
     # is created which causes problems in the next steps
     """
-    try:    
+    try:
         filegdict = ShakeGrid.getFileGeoDict(filename, adjust='res')
     except:
         try:
@@ -491,8 +491,6 @@ class TempHdf(object):
         rowends = np.tile(rowen, len(colen))
         colends = np.repeat(colen, len(rowen))
         return rowstarts, rowends, colstarts, colends
-        
-
 
 
 class LogisticModel(object):
@@ -833,6 +831,12 @@ class LogisticModel(object):
             # Apply slope min/max limits
             print('applying slope thresholds')
             P = P * self.nonzero
+            P[np.isnan(P)] = 0.0
+            if self.uncert is not None:
+                Pmin = Pmin * self.nonzero
+                Pmax = Pmax * self.nonzero
+                Pmin[np.isnan(Pmin)] = 0.0
+                Pmax[np.isnan(Pmax)] = 0.0
         else:
             print('No slope file provided, slope thresholds not applied')
         # Stuff into Grid2D object
@@ -847,16 +851,16 @@ class LogisticModel(object):
         Pgrid = Grid2D(P, self.geodict)
         rdict = collections.OrderedDict()
         rdict['model'] = {'grid': Pgrid,
-                          'label': ('%s %s') % (self.modeltype.capitalize(), units5.capitalize()),
+                          'label': ('%s %s') % (self.modeltype.capitalize(), units5.title()),
                           'type': 'output',
                           'description': description}
         if self.uncert is not None:
             rdict['modelmin'] = {'grid': Grid2D(Pmin, self.geodict),
-                                 'label': ('%s %s (-%0.1f std ground motion)') % (self.modeltype.capitalize(), units5.capitalize(), self.numstd),
+                                 'label': ('%s %s (-%0.1f std ground motion)') % (self.modeltype.capitalize(), units5.title(), self.numstd),
                                  'type': 'output',
                                  'description': description}
             rdict['modelmax'] = {'grid': Grid2D(Pmax, self.geodict),
-                                 'label': ('%s %s (+%0.1f std ground motion)') % (self.modeltype.capitalize(), units5.capitalize(), self.numstd),
+                                 'label': ('%s %s (+%0.1f std ground motion)') % (self.modeltype.capitalize(), units5.title(), self.numstd),
                                  'type': 'output',
                                  'description': description}
         # This step might swamp memory for higher resolution runs

groundfailure/makemaps.py
@@ -746,10 +746,10 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
         if k == 0:
             wid, ht = axsize.width, axsize.height
         default1 = True
-        if len(colormaps) == 1 and len(newgrids) == 1:
+        if len(colormaps) == 1 and len(plotorder) == 1:
             palette = colormaps
             default1 = False
-        if colormaps is not None and len(colormaps) == len(newgrids) and colormaps[k] is not None:
+        if colormaps is not None and len(colormaps) == len(plotorder) and colormaps[k] is not None:
             palette = colormaps[k]
             default1 = False
         if default1:  # Find preferred default color map for each type of layer if no colormaps found
@@ -789,13 +789,13 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
 
         # mask out anything below any specified thresholds
 
-        # if logscale is not False and len(logscale) == len(newgrids):
+        # if logscale is not False and len(logscale) == len(plotorder):
         #     if logscale[k] is True:
         #         dat = np.log10(dat)
         #         label1 = r'$log_{10}$(' + label1 + ')'
 
         if scaletype.lower() == 'binned':
-            if logscale is not False and len(logscale) == len(newgrids):
+            if logscale is not False and len(logscale) == len(plotorder):
                 if logscale[k] is True:
                     clev = 10.**(np.arange(np.floor(np.log10(np.nanmin(dat))), np.ceil(np.log10(np.nanmax(dat))), 0.25))
                 else:
@@ -806,7 +806,7 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
                     else:
                         scal = 1.
 
-                    if lims is None or len(lims) != len(newgrids):
+                    if lims is None or len(lims) != len(plotorder):
                         clev = (np.linspace(np.floor(scal*np.nanmin(dat)), np.ceil(scal*np.nanmax(dat)), 10))/scal
                     else:
                         if lims[k] is None:
@@ -820,8 +820,7 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
                     scal = 10**-order
                 else:
                     scal = 1.
-
-                if lims is None or len(lims) != len(newgrids):
+                if lims is None or len(lims) != len(plotorder):
                     clev = (np.linspace(np.floor(scal*np.nanmin(dat)), np.ceil(scal*np.nanmax(dat)), 10))/scal
                 else:
                     if lims[k] is None:
@@ -843,7 +842,8 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
             if isinstance(logscale, (bool)):
                 # Put it in a list so it won't break things later
                 logscale = [logscale]
-            if lims is not None and len(lims) == len(newgrids):
+
+            if lims is not None and len(lims) == len(plotorder):
                 if lims[k] is None:
                     vmin = np.nanmin(dat)
                     vmax = np.nanmax(dat)
@@ -855,7 +855,7 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
                 vmax = np.nanmax(dat)
 
         # Might need to move this up to before downsampling...might give illusion of no hazard in places where there is some that just got averaged out
-        if maskthreshes is not None and len(maskthreshes) == len(newgrids):
+        if maskthreshes is not None and len(maskthreshes) == len(plotorder):
             if maskthreshes[k] is not None:
                 dat[dat <= maskthreshes[k]] = float('NaN')
                 dat = np.ma.array(dat, mask=np.isnan(dat))
@@ -925,7 +925,7 @@ def modelMap(grids, shakefile=None, suptitle=None, inventory_shapefile=None,
         # add colorbar
         cbfmt = '%1.2f'
         if vmax is not None and vmin is not None:
-            if logscale is not False and len(logscale) == len(newgrids):
+            if logscale is not False and len(logscale) == len(plotorder):
                 if logscale[k] is True:
                     cbfmt = None
             elif (vmax - vmin) < 1.:
@@ -1253,6 +1253,7 @@ def interactiveMap(grids, shakefile=None, plotorder=None, inventory_shapefile=No
         palette.set_bad(clear_color, alpha=0.0)
 
         dat = grid.getData().copy()
+        dat[dat == 0.] = float('nan')  # Makes zero probability areas clear
 
         if scaletype.lower() == 'binned':
             if logscale is not False and len(logscale) == len(plotorder):